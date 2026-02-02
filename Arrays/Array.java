9bc33bc7ca0ec8a50cba3e44641fa6efcfe10978
Yash Karande <yashkarande4240@gmail.com>
2026-02-02 22:50:34 +0530

    Q3 check if array sorted

--- Arrays/Array.java
+++ Arrays/Array.java
@@ -1,12 +1,17 @@
 public class Array {
 
   public static void main(String[] args) {
-    int[] arr = new int[]{1, 5, 8, 2, 0, 9, 5, 3, 55};
-    System.out.println(maxElement(arr));
-    reverseArray(arr);
-    for (int i = 0; i < arr.length; i++) {
-      System.out.print(arr[i] + " ");
-    }
+    // int[] arr = new int[]{1, 5, 8, 2, 0, 9, 5, 3, 55};
+    int[] arr = new int[]{1, 3, 5, 8, 8};
+    // System.out.println(maxElement(arr));
+    // reverseArray(arr);
+    // for (int i = 0; i < arr.length; i++) {
+    //   System.out.print(arr[i] + " ");
+    // }
+
+    System.out.println(isSorted(arr));
+
+    
   }
 
   // Q1. Find Maximum Element
@@ -32,6 +37,16 @@ public class Array {
     }
 
     
+  }
+
+  // Q3. Check if array sorted
+  public static boolean isSorted(int[] arr) {
+    for(int i = 1; i < arr.length; i++) {
+      if (arr[i] < arr[i - 1]) {
+        return false;
+      }
+    }
+    return true;
   }
 
 
