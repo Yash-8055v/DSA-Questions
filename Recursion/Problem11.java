9fc1edaa38cec0b44cfec812b4d6ab0f3de9bca4
Yash Karande <yashkarande4240@gmail.com>
2026-04-29 00:39:18 +0530

    Problem11: Binary String Problem

---
+++ Recursion/Problem11.java
@@ -0,0 +1,21 @@
+//* Print all binary strings of size N without consecutive ones */
+
+public class Problem11 {
+  public static void main(String[] args) {
+    printBinString(3, 0, "");
+  }
+
+  public static void printBinString(int n, int lastPlace, String str) {
+    //* base case */
+    if (n == 0) {
+      System.out.println(str);
+      return;
+    }
+
+    //* Work */
+    printBinString(n - 1, 0, str+"0");
+    if (lastPlace == 0) {
+      printBinString(n - 1, 1, str+"1");
+    }
+  }
+}
