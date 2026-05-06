c7aa3e020158629a8e8a616aa9eedc784d38f00f
Yash Karande <yashkarande4240@gmail.com>
2026-05-07 00:44:17 +0530

    Remove node from last in LL

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -89,6 +89,30 @@ public class LinkedList {
     return val;
   }
 
+  // Remove last in LL
+  public int removeLast() {
+    if (size == 0) {
+      System.out.println("LL is empty");
+      return Integer.MIN_VALUE;
+    }else if (size == 1) {
+      int val = head.data;
+      head = tail = null;
+      size = 0;
+      return val;
+    }
+
+    Node prev = head;
+    for(int i = 0; i < size - 2; i++ ) {
+      prev = prev.next;
+    }
+
+    int val = prev.next.data;
+    prev.next = null;
+    tail = prev;
+    size--;
+    return val;
+  }
+
   // Printing LL
   public void printLL() {
     Node temp = head;
