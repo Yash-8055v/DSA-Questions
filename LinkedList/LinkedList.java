21946124fa95ff456d79847c8decb2e32eff53a4
Yash Karande <yashkarande4240@gmail.com>
2026-05-06 23:54:32 +0530

    Adding Node at start of LL

---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,38 @@
+package LinkedList;
+
+public class LinkedList {
+  public static class Node {
+  
+    int data;
+    Node next;
+
+    public Node(int data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+  public static Node head;
+  public static Node tail;
+
+  // Adding Node at start
+  public void addFirst(int data) {
+    // step 1: create new node
+    Node newNode = new Node(data);
+
+    if(head == null) {
+      head = tail = newNode;
+      return;
+    }
+
+    // step 2: newNode next = head
+    newNode.next = head;
+
+    // step 3: head = newNode
+    head = newNode;
+  }
+
+  public static void main(String[] args) {
+    LinkedList ll = new LinkedList();
+  }
+}
