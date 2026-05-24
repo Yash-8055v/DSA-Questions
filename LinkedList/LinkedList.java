b9680c2afebc57d6df1ccfb7b94cd93ff57b427a
Yash Karande <yashkarande4240@gmail.com>
2026-05-24 07:52:19 +0530

    Remove cycle form LL

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -262,6 +262,37 @@ public class LinkedList {
     return false; // cycle not exist
   }
 
+  // Remove cycle form LL
+  public static void removeCycle() {
+    // detect cycle
+    Node slow = head;
+    Node fast = head;
+    boolean cycle = false;
+
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (fast == slow) {
+        cycle = true;
+        break;
+      }
+    }
+    if (cycle == false) {
+      return;
+    }
+
+    // find meeting point
+    slow = head;
+    Node prev = null;
+    while (slow != fast) {
+      prev = fast;
+      slow = slow.next;
+      fast = slow.next;
+    }
+
+    // remove cycle 
+    prev.next =  null;
+  }
 
 
   // Printing LL
