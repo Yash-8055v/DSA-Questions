5a92c7e10f6224d64834762842f5f06f25c662b4
Yash Karande <yashkarande4240@gmail.com>
2026-06-13 13:27:54 +0530

    Queue Q5: Queue Reversal

---
+++ Queue/Question5.java
@@ -0,0 +1,41 @@
+package Queue;
+
+// Queue Q5: Reverse a Queue
+// TC: O(n)
+// SC: O(n)
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Question5 {
+
+  public static void reverse(Queue<Integer> q) {
+    Stack<Integer> s = new Stack<>();
+
+    while(!q.isEmpty()) {
+      s.push(q.remove());
+    }
+
+    while (!s.isEmpty()) {
+      q.add(s.pop());
+    }
+  } 
+  
+  public static void main(String[] args) {
+    Queue<Integer> q = new LinkedList<>();
+    q.add(1);
+    q.add(2);
+    q.add(3);
+    q.add(4);
+    q.add(5);
+
+    reverse(q);
+
+    // print queue
+    while (!q.isEmpty()) {
+      System.out.print(q.remove() + " ");
+    }
+
+  }
+}
