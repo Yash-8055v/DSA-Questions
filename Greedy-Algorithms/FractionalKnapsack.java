749c067a8ad8a6ab4ecc545ada4949051693a734
Yash Karande <yashkarande4240@gmail.com>
2026-06-17 07:28:00 +0530

    Fractional Knapsack

---
+++ Greedy-Algorithms/FractionalKnapsack.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+// Fractional Knapsack
+// TC: O(n log n)
+// SC: O(n)
+
+public class FractionalKnapsack {
+  
+
+  public static void main(String[] args) {
+    int val[] = {60, 100, 120};
+    int weight[] = {10, 20, 30};
+    int W = 50;
+
+    double ratio[][] = new double[val.length][2];
+    // 0th col - idx ; 1st col - ratio
+
+    for(int i = 0; i < val.length; i++) { 
+      ratio[i][0] = i;
+      ratio[i][1] = val[i] / (double) weight[i];
+    }
+
+    // ascending order
+    Arrays.sort(ratio, Comparator.comparingDouble(o -> o[1]));
+
+    int capacity = W;
+    int finalValue = 0;
+
+    // for descending order traverse in reverse order
+
+    for(int i = ratio.length - 1; i >= 0; i--){
+      int idx = (int)ratio[i][0];
+      if (capacity >= weight[idx]) {
+        finalValue += val[idx];
+        capacity -= weight[idx];
+      }else{
+        finalValue += (ratio[i][1] * capacity);
+        capacity = 0;
+        break;
+      }
+    }
+
+    System.out.println("Final value = " + finalValue);
+
+  }
+}
