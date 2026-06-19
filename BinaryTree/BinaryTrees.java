5ac1c0978737fac4e09767b048f14a9ee4b211e4
Yash Karande <yashkarande4240@gmail.com>
2026-06-19 23:20:32 +0530

    level order traversal

--- BinaryTree/BinaryTrees.java
+++ BinaryTree/BinaryTrees.java
@@ -85,6 +85,41 @@ public class BinaryTrees {
       
     } 
 
+    // Level Order Traversal
+    // TC: O(n)
+    public static void levelOrder(Node root) {
+      if (root == null) {
+        return;
+      }
+
+      Queue<Node> q = new LinkedList<>();
+      q.add(root);
+      q.add(null);
+
+      while (!q.isEmpty()) {
+        Node currNode = q.remove();
+        if (currNode == null) {
+          System.out.println();
+          if (q.isEmpty()) {
+            break;
+          }else {
+            q.add(null);
+          }
+        }else{
+          System.out.print(currNode.data + " ");
+          if (currNode.left != null) {
+            q.add(currNode.left);
+          }
+          if (currNode.right != null) {
+            q.add(currNode.right);
+          }
+        }
+
+      }
+
+      
+    }
+
     
 
 
