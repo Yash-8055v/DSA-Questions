8d377542beff3cfeb22117f64bb58aad15ed44cc
Yash Karande <yashkarande4240@gmail.com>
2026-06-26 23:24:33 +0530

    Subtree of another Tree

--- BinaryTree/BinaryTrees.java
+++ BinaryTree/BinaryTrees.java
@@ -185,6 +185,43 @@ public class BinaryTrees {
     }
 
 
+    // Subtree of another Tree
+    
+    public static boolean isIdentical(Node node, Node subRoot) {
+      if (node == null && subRoot == null) {
+        return true;
+      } else if (node == null || subRoot == null || node.data != subRoot.data)  {
+        return false;
+      }
+
+      if (!isIdentical(node.left, subRoot.left)) {
+        return false;
+      }
+
+      if (!isIdentical(node.right, subRoot.right)) {
+        return false;
+      }
+
+      return true;
+    }
+
+    public static boolean isSubtree(Node root, Node subRoot) {
+      if (root == null) {
+        return false;
+      }
+
+      if (root.data == subRoot.data) {
+        if (isIdentical(root, subRoot)) {
+          return true;
+        }
+      }
+
+      
+      
+
+      return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
   public static void main(String[] args) {
     int nodes[] = {1, 2, 3, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
     Node root = BinaryTree.buildTree(nodes);
