73b25bc126418a96ede76bfdaa94e7afa6507e89
Yash Karande <yashkarande4240@gmail.com>
2026-06-27 12:50:31 +0530

    Top View of a Tree

--- BinaryTree/BinaryTrees.java
+++ BinaryTree/BinaryTrees.java
@@ -1,5 +1,6 @@
 package BinaryTree;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -220,6 +221,59 @@ public class BinaryTrees {
       
 
       return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    // Top View of a Tree
+    static class Info2 {
+      Node node;
+      int hd;
+
+      public Info2(Node node, int hd) {
+        this.node = node;
+        this.hd = hd;
+      }
+    }
+
+    public static void topView(Node root) {
+      // level order 
+      Queue<Info2> q = new LinkedList<>();
+      HashMap<Integer, Node> map = new HashMap<>();
+
+      int min = 0, max = 0; 
+      q.add(new Info2(root, 0));
+      q.add(null);
+
+      while (!q.isEmpty()) {
+        Info2 curr = q.remove();
+        if (curr == null) {
+          if (q.isEmpty()) {
+            break;
+          } else {
+            q.add(null);
+          }
+        } else {
+          if (!map.containsKey(curr.hd)) {
+          map.put(curr.hd, curr.node);
+        }
+
+        if (curr.node.left != null) {
+          q.add(new Info2(curr.node.left, curr.hd - 1));
+          min = Math.min(min, curr.hd-1);
+        }
+
+        if (curr.node.right != null) {
+          q.add(new Info2(curr.node.right, curr.hd+1));
+          max = Math.max(max, curr.hd+1);
+        }
+        }
+
+        
+      }
+
+      for(int i = min; i <= max; i++) {
+        System.out.print(map.get(i).data + " ");
+      }
+
     }
 
   public static void main(String[] args) {
