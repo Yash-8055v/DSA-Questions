84cf2b277f94c293fe1778e452a1bf48bc3e181a
Yash Karande <yashkarande4240@gmail.com>
2026-06-27 21:52:16 +0530

    Lowest Common Ancestor (LCA): Approach 1

--- BinaryTree/BinaryTrees.java
+++ BinaryTree/BinaryTrees.java
@@ -1,5 +1,6 @@
 package BinaryTree;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -331,6 +332,50 @@ public class BinaryTrees {
       KthLevel2(root.right, k, level+1);
     }
 
+    // Lowest Common Ancestor (LCA): Approach 1
+    // TC: O(n)
+    // SC: O(n)
+    public static boolean getPath(Node root, int n, ArrayList<Node> path){
+      if (root == null) {
+        return false;
+      }
+
+      path.add(root);
+
+      if (root.data == n) {
+        return true;
+      }
+
+      boolean foundLeft = getPath(root.left, n, path);
+      boolean foundRight = getPath(root.right, n, path);
+
+      if (foundLeft || foundRight) {
+        return true;
+      }
+
+      path.remove(path.size() - 1);
+      return false;
+    }
+    public static Node lca(Node root, int n1, int n2) {
+      ArrayList<Node> path1 = new ArrayList<>();
+      ArrayList<Node> path2 = new ArrayList<>();
+
+      getPath(root, n1, path1);
+      getPath(root, n2, path2);
+
+      // lca
+      int i = 0;
+      for(; i < path1.size() && i < path2.size(); i++) {
+        if (path1.get(i) != path2.get(i)) {
+          break;
+        }
+      }
+
+      // lase equal node -> i - 1
+      Node lca = path1.get(i - 1);
+
+      return lca;
+    } 
   
 
   public static void main(String[] args) {
