92a0eb8d6d14a32db29b83828546a2a5c9c89ffe
Yash Karande <yashkarande4240@gmail.com>
2026-06-28 19:01:31 +0530

    Delete a node in BST

--- BinarySearchTree_BST/BST.java
+++ BinarySearchTree_BST/BST.java
@@ -54,6 +54,43 @@ public class BST {
     
   }
 
+  // Delete a node in BST
+  // TC: O(H)
+  public static Node findInorderSuccessor(Node root) {
+    while (root.left != null) {
+      root = root.left;
+    }
+    return root;
+  }
+
+
+  public static Node delete(Node root, int val) {
+    if (root.data < val) {
+      root.right = delete(root.right, val);
+    }else if (root.data > val) {
+      root.left = delete(root.left, val);
+    }else {
+      // case 1: leaf node
+      if (root.left == null && root.right == null) {
+        return null;
+      }
+
+      // case 2: single child
+      if (root.left == null) {
+        return root.right;
+      }else if (root.right == null) {
+        return root.left;
+      }
+
+      // case 3: two children
+      Node IS = findInorderSuccessor(root.right);
+      root.data = IS.data;
+      root.right = delete(root.right, IS.data);
+    }
+
+    return root;
+  }
+
   public static void inorder(Node root) {
     if (root == null) {
       return;
