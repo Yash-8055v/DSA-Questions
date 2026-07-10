dd795e5b94c7b80a753d44002cf50f283a9e090c
Yash Karande <yashkarande4240@gmail.com>
2026-07-11 05:25:49 +0530

    Delete from Heap

--- Heaps/HeapDataStructure.java
+++ Heaps/HeapDataStructure.java
@@ -31,6 +31,52 @@ public class HeapDataStructure {
       return arr.get(0);
     }
 
+    // Delete from Heap
+    // TC: O(log n)
+
+    private void heapify(int idx) {
+      int left = 2*idx+1;
+      int right = 2*idx+2;
+      int minIdx = idx;  
+
+      // check if left child is min
+      if (left < arr.size() && arr.get(minIdx) > arr.get(left)) {
+        minIdx = left;
+      }
+
+      // check if right child is min
+      if (right < arr.size() && arr.get(minIdx) > arr.get(right)) {
+        minIdx = right;
+      }
+
+      if (minIdx != idx) {  // if root is not min then only swap otherwise if root is min no need of swap
+
+        int temp = arr.get(idx);
+        arr.set(idx, arr.get(minIdx));
+        arr.set(minIdx, temp);
+
+        heapify(minIdx); // now again call for next level
+        
+      }
+    }
+    public int remove() {
+      int data = arr.get(0); // get root(element to delete) element
+
+      // Delete &  Heap fix logic
+      
+      // 1. swap first and last 
+      int temp = arr.get(0);
+      arr.set(0, arr.size() - 1);
+      arr.set(arr.size() - 1, temp);
+
+      // 2. delete last
+      arr.remove(arr.size() - 1);
+
+      // 3. heapify
+      heapify(0);
+      return data; // return deleted element
+    }
+
   }
   
   public static void main(String[] args) {
